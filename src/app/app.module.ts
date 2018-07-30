d95fd683410dea2ac57e49da257116c849133789
Mohamadou Ly <lymouhamad@gmail.com>
2018-07-30 19:56:51 -0400

    father cleanup by separating the recipes list with the single recipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
+import { RecipeSummaryComponent } from './components/recipe-summary/recipe-summary.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipeListComponent
+    RecipeListComponent,
+    RecipeSummaryComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/recipe-summary/recipe-summary.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../../../model/recipe';
+
+@Component({
+  selector: 'app-recipe-summary',
+  templateUrl: './recipe-summary.component.html',
+  styleUrls: ['./recipe-summary.component.css']
+})
+export class RecipeSummaryComponent implements OnInit {
+
+  @Input() recipe: Recipe;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
